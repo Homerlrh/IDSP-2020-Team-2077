0c5c540c06798d4aa54dd064ffef2758e65f132a
sahilsian <sahilsian101@gmail.com>
2020-03-08 01:52:31 -0800

    Added Draft Nav Bar/Bottom Icon Menu

---
+++ app.js
@@ -0,0 +1,20 @@
+let state = {};
+
+fetch("./objects.json")
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        state = data.boolstate
+    })
+
+function menutoggle() {
+    if(state === false) {
+        state = true;
+        document.querySelector("#js-menu-dropdown").style.display = "flex";
+    } else {
+        state = false;
+        document.querySelector("#js-menu-dropdown").style.display = "none";
+    }
+}
